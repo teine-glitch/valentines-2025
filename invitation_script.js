f644eb9bd765d8a0e6c03aec002fa8991d9edaf0
teine-glitch <evascoramsteine@gmail.com>
2025-02-08 18:09:50 +0800

    Add files via upload

---
+++ invitation_script.js
@@ -0,0 +1,4 @@
+/* script.js */
+function openWebsite() {
+    window.open("https://app.sane.fyi/s/3899a130-e58a-11ef-8a8e-df0c79a6ae58?t=5Dl6tolJU1dlQwcWchXz&utm_source=share-menu&utm_medium=web", "_blank");
+}
